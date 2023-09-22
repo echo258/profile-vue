ae45d3371fc2e90bc8a992474da94d762039e6fa
Jason <jape258@gmail.com>
2023-09-22 11:43:44 -0400

    google

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import LottieVuePlayer from '@lottiefiles/vue-lottie-player'
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(LottieVuePlayer)
 Vue.config.productionTip = false
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'G-W9PXHDNQMF'
+  router
+});
 
 new Vue({
   render: h => h(App),
